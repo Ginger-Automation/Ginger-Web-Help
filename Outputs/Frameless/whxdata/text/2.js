455cfa092fa806eb0e51bde7a34e61a98e439469
Vijaya2110-github <vijaya.sapkale@amdocs.com>
2021-06-16 15:06:21 +0530

    initial setup

---
+++ Outputs/Frameless/whxdata/text/2.js
@@ -0,0 +1,1 @@
+rh._.exports({"0":[["Plugins"]],"1":[["\n  ","\n    "],["\n    "," ","\n    ","Plugins are required in the solution like the PACT are be added from this place.","\n    "," ","\n    ","\n  ","\n\n"]],"2":[["Plugins "]],"id":"2"})
