e69cba23e484c7a7e70ac8b551b5c2ccbcb46c29
Vijaya2110-github <vijaya.sapkale@amdocs.com>
2021-07-09 23:20:30 +0530

    Changes done on 9th July

--- docs/whxdata/projectsettings.js
+++ docs/whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("First_Topic.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.html'));
 rh.consts('START_FILEPATH', encodeURI('index.html'));
-rh.consts('HELP_ID', '730879cc-9d61-4546-887e-c342d8663074' || 'preview');
+rh.consts('HELP_ID', '867fdbdf-3563-441e-8f4e-82da6739de10' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "en");
