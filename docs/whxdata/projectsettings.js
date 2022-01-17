12d5506d87f3d5c3d1d780e85a3850dcec04706c
Vijaya2110-github <vijaya.sapkale@amdocs.com>
2022-01-17 14:07:25 +0530

    Changes 17th Jan

--- docs/whxdata/projectsettings.js
+++ docs/whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("First_Topic.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.html'));
 rh.consts('START_FILEPATH', encodeURI('index.html'));
-rh.consts('HELP_ID', 'ceebb365-115e-45ec-bbad-f28fa8f524e9' || 'preview');
+rh.consts('HELP_ID', '076e749e-82c9-47ac-a3e9-3820b17c584b' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "en");
