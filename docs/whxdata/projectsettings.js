a9bea9dd5eb626a3ae44d6e07f41b546e5cc30a2
Himanshu Chauhan <himanshu.chauhan@amdocs.com>
2023-12-06 17:43:06 +0530

    Signed-off-by: Himanshu Chauhan <himanshu.chauhan@amdocs.com>

--- docs/whxdata/projectsettings.js
+++ docs/whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("First_Topic.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.html'));
 rh.consts('START_FILEPATH', encodeURI('index.html'));
-rh.consts('HELP_ID', '67bd11a9-dc09-4561-81f0-569e9f7c3bc9' || 'preview');
+rh.consts('HELP_ID', 'dd03137c-9ff8-4b18-aa38-16aa9a1d2492' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "en");
